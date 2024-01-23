13cfc60dfea731d2da949ad0d31db989fc99a2dc
CodeAbhii <hackscoc8317@gmail.com>
2024-01-23 23:03:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     // document.querySelector(".header_area").style.position = "fixed";
+//     document.querySelector(".header_area").style.position = "fixed";
+//     document.querySelector(".header_area").style.width = "100%";
+//     document.querySelector(".header_area").style.zIndex = "10";
+//   } else {
+//     document.querySelector(".header_area").style.position = "static";
+//   }
+// }
+
+// // Dropdown
+// const toggleDropDown = document.querySelector(".toggle_dropdown");
+// const dropdown = document.querySelector(".dropdown_menu");
+
+
